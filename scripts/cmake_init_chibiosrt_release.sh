c937862fe2faf553438fbbd366a51d570475d806
Daniel Jerolm <daniel.jerolm@web.de>
2022-10-10 22:21:20 +0200

    Updated due to change at toolchain_configs

--- scripts/cmake_init_chibiosrt_release.sh
+++ scripts/cmake_init_chibiosrt_release.sh
@@ -6,6 +6,6 @@ cd ..
 
 rm -fr build_chibiosrt
 
-cmake -S . -B build_chibiosrt -DCMAKE_TOOLCHAIN_FILE=toolchain_configs/cross_gcc/settings.arm-none-eabi-posix.cortex-m3.cmake -DGPCCDEV_Artifact:STRING=productive_chibios_arm -DCMAKE_BUILD_TYPE=custom_release
+cmake -S . -B build_chibiosrt -DCMAKE_TOOLCHAIN_FILE=toolchain_configs/cross_gcc/toolchain.arm-none-eabi-posix.cortex-m3.cmake -DGPCCDEV_Artifact:STRING=productive_chibios_arm -DCMAKE_BUILD_TYPE=custom_release
 
 echo "Done"

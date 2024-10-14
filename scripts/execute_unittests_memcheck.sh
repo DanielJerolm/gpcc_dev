27c8b126b329b5461459a849d2e82090a672583a
Daniel Jerolm <daniel.jerolm@web.de>
2024-10-14 13:22:00 +0200

    Skript for unittest with memcheck accepts filter now

--- scripts/execute_unittests_memcheck.sh
+++ scripts/execute_unittests_memcheck.sh
@@ -4,9 +4,13 @@
 # If a copy of the MPL was not distributed with this file,
 # You can obtain one at https://mozilla.org/MPL/2.0/.
 #
-# Copyright (C) 2022 Daniel Jerolm
+# Copyright (C) 2022, 2024 Daniel Jerolm
 
 set -e
 
 cd ../build_unittest/output
-valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all ./unittests --gtest_filter=-*Death*:-*DeferredCancel*
+if [ $# -eq 0 ]; then
+  valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all ./unittests --gtest_filter=-*Death*:-*DeferredCancel*
+else
+  valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all ./unittests $@
+fi
